b0b8fa5194196d924cee4144e573d6ccc6b369e4
nabilah24 <nabilah.zakiyah.2205356@students.um.ac.id>
2024-09-16 15:48:37 +0700

    landing page

---
+++ challenge-1/assets/js/script.js
@@ -0,0 +1,45 @@
+(function ($) {
+  "use strict";
+
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: false,
+    smartSpeed: 1000,
+    center: true,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>'
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      }
+    }
+  });
+
+
+})(jQuery);
+
